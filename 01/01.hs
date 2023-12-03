82aa5a27a448c27b4d51c74593ddb2ad384ce70c
Liam van der Vyver <91105007+liamvdvyver@users.noreply.github.com>
2023-12-03 20:47:02 +1100

    refactor(01): camelCase

--- 01/01.hs
+++ 01/01.hs
@@ -2,33 +2,33 @@ module Main where
 import System.IO
 import Control.Monad
 
-firstnum, lastnum :: String -> Char
+firstNum, lastNum :: String -> Char
 
-firstnum line = firstnum' line 0
+firstNum line = firstNum' line 0
     where
-        firstnum' :: String -> Int -> Char
-        firstnum' line pos
+        firstNum' :: String -> Int -> Char
+        firstNum' line pos
             | [line!!pos] `elem` (map show [0..9]) = line!!pos
-            | pos < length line                    = firstnum' line (pos + 1)
+            | pos < length line                    = firstNum' line (pos + 1)
 
-lastnum line = lastnum' line (length line - 1)
+lastNum line = lastNum' line (length line - 1)
     where
-        lastnum' :: String -> Int -> Char
-        lastnum' line pos
+        lastNum' :: String -> Int -> Char
+        lastNum' line pos
             | [line!!pos] `elem` (map show [0..9]) = line!!pos
-            | pos >= 0                             = lastnum' line (pos - 1)
+            | pos >= 0                             = lastNum' line (pos - 1)
 
-linenum :: String -> Int
-linenum line = read ((firstnum line) : (lastnum line) : []) :: Int
+lineNum :: String -> Int
+lineNum line = read ((firstNum line) : (lastNum line) : []) :: Int
 
-nums_sum :: [Int] -> Int
-nums_sum xs = nums_sum' xs
+numsSum :: [Int] -> Int
+numsSum xs = numsSum' xs
     where
-        nums_sum' :: [Int] -> Int
-        nums_sum' [] = 0
-        nums_sum' (x:xs) = x + nums_sum' xs
+        numsSum' :: [Int] -> Int
+        numsSum' [] = 0
+        numsSum' (x:xs) = x + numsSum' xs
 
-spelled_nums = [
+spelledNums = [
     ("one",   "1"),
     ("two",   "2"),
     ("three", "3"),
@@ -41,7 +41,7 @@ spelled_nums = [
     ] ++ (zip (map show [1..9]) (map show [1..9]))
 
 isSpelledNum :: String -> Bool
-isSpelledNum string = string `elem` map fst spelled_nums
+isSpelledNum string = string `elem` map fst spelledNums
 
 getSpelledNumTuple :: String -> (String, String)
 getSpelledNumTuple string
@@ -51,7 +51,7 @@ getSpelledNumTuple string
     where
         min_num_length' = 3
         getSpelledNum' :: String -> (String, String)
-        getSpelledNum' string = (filter (\x -> fst x == string) spelled_nums)!!0
+        getSpelledNum' string = (filter (\x -> fst x == string) spelledNums)!!0
 
 -- quadratic time, could be better
 spellOutLine :: String -> Int -> String
@@ -61,17 +61,16 @@ spellOutLine line pos
         let leftPart  = if pos == 0 then "" else (map (line!!) [0..(pos - 1)])
         let rightPart = (map (line!!) [pos..(length line - 1)])
         let spelledNumTuple = getSpelledNumTuple rightPart
-        -- let spelledNumReplacementLength = length (snd spelledNumTuple) -- 1
         let rightPartTrunc = map (rightPart!!) [1..(length rightPart - 1)]
         let lineNew = leftPart ++ (snd spelledNumTuple) ++ rightPartTrunc
         let posNew = pos + 1
         spellOutLine lineNew posNew
 
 solvePartOne :: [String] -> Int
-solvePartOne lines = nums_sum $ map linenum lines
+solvePartOne lines = numsSum $ map lineNum lines
 
 solvePartTwo :: [String] -> Int
-solvePartTwo lines = nums_sum $ map linenum $ map (\x -> spellOutLine x 0) lines
+solvePartTwo lines = numsSum $ map lineNum $ map (\x -> spellOutLine x 0) lines
 
 main :: IO ()
 main = do
