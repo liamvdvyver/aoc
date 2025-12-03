fa139be14a8e40c66544391caea0ffd90f392a85
Liam van der Vyver <91105007+liamvdvyver@users.noreply.github.com>
2025-12-03 15:52:55 +1100

    feat: grid class for today

--- libOc/libOc/grid.h
+++ libOc/libOc/grid.h
@@ -1,131 +1,75 @@
-#pragma once
-
-#include "iostream"
-#include <cassert>
-#include <cstdint>
+#include <iostream>
 #include <vector>
 
-typedef int idx_t;
-
-// COORDINATES
-
-typedef struct {
-  idx_t x;
-  idx_t y;
-} coord_t;
-
-constexpr coord_t operator+(const coord_t a, const coord_t b) {
-  return {a.x + b.x, a.y + b.y};
+using Coord = std::pair<size_t, size_t>;
+constexpr Coord operator+(Coord a, Coord b) {
+  return std::make_pair(a.first + b.first, a.second + b.second);
 }
 
-constexpr coord_t operator-(const coord_t a, const coord_t b) {
-  return {a.x - b.x, a.y - b.y};
-}
-
-constexpr bool operator<(const coord_t a, const coord_t b) {
-  return a.x < b.x && a.y < b.y;
-}
-
-constexpr bool operator==(const coord_t a, const coord_t b) {
-  return a.x == b.x && a.y == b.y;
-}
-
-constexpr bool operator!=(const coord_t a, const coord_t b) {
-  return !(a == b);
-}
-
-constexpr bool operator>(const coord_t a, const coord_t b) { return b < a; }
-
-constexpr bool operator<=(const coord_t a, const coord_t b) {
-  return a.x <= b.x && a.y <= b.y;
-}
-
-constexpr bool operator>=(const coord_t a, const coord_t b) {
-  return a.x >= b.x && a.y >= b.y;
-}
-
-constexpr coord_t operator*(const coord_t a, const idx_t c) {
-  return {a.x * c, a.y * c};
-};
-
-constexpr coord_t operator*(const idx_t c, const coord_t a) { return a * c; };
-
-constexpr coord_t operator/(const coord_t a, const idx_t c) {
-  return {a.x / c, a.y / c};
-};
-
-std::ostream &operator<<(std::ostream &os, const coord_t a);
-
-// DIRECTIONS
-
-enum class Direction : uint8_t {
-
-  UP = 0b1,
-  DOWN = 0b10,
-  LEFT = 0b100,
-  RIGHT = 0b1000,
-
-  UP_LEFT = 0b10000,
-  UP_RIGHT = 0b100000,
-  DOWN_LEFT = 0b1000000,
-  DOWN_RIGHT = 0b10000000,
-
-};
-
-constexpr coord_t offset(Direction d) {
-  switch (d) {
-  case Direction::UP:
-    return {0, -1};
-  case Direction::DOWN:
-    return {0, 1};
-  case Direction::LEFT:
-    return {-1, 0};
-  case Direction::RIGHT:
-    return {1, 0};
-  case Direction::UP_LEFT:
-    return offset(Direction::UP) + offset(Direction::LEFT);
-  case Direction::UP_RIGHT:
-    return offset(Direction::UP) + offset(Direction::RIGHT);
-  case Direction::DOWN_LEFT:
-    return offset(Direction::DOWN) + offset(Direction::LEFT);
-  case Direction::DOWN_RIGHT:
-    return offset(Direction::DOWN) + offset(Direction::RIGHT);
-    break;
-  }
-}
-
-std::vector<coord_t> row_major(coord_t ub);
+using C = Coord;
 
 template <typename T> struct Grid {
 
-  coord_t bounds;
-  T *items;
+  constexpr Grid(std::vector<std::vector<T>> v) : v(v) {}
 
-public:
-  Grid<T>(coord_t c) : bounds{c}, arr_sz{(size_t)c.x * c.y} {
-    items = new T[arr_sz];
-    std::fill(items, items + arr_sz, 0);
+  constexpr Grid() {}
+
+  std::vector<std::vector<T>> v;
+
+  void pad(T val) {
+    const size_t n_x = bound().first;
+
+    for (auto &u : v) {
+      u.insert(u.begin(), val);
+      u.push_back(val);
+    }
+
+    v.emplace(v.begin(), n_x + 2, val);
+    v.emplace_back(n_x + 2, val);
   }
 
-  T &at(coord_t c) {
-    size_t idx = c.y * bounds.x + c.x;
-    assert(idx < arr_sz);
-    assert(c < bounds);
-
-    return items[idx];
-  }
-  T &at(coord_t c) const {
-    size_t idx = c.y * bounds.x + c.x;
-    assert(idx < arr_sz);
-    assert(c < bounds);
-
-    return items[idx];
+  Coord bound() {
+    if (v.size() == 0)
+      return std::make_pair(0, 0);
+    return std::make_pair(v[0].size(), v.size());
   }
 
-private:
-  size_t arr_sz;
+  // Accessors
+
+  T &operator[](const Coord c) { return v[c.first][c.second]; }
+
+  const T &operator[](const Coord c) const { return v.at(c.first)(c.second); }
+
+  // Printing
+  friend std::ostream &operator<<(std::ostream &os, const Grid &g) {
+    for (auto &u : g.v) {
+      for (auto e : u) {
+        os << e;
+      }
+      os << '\n';
+    }
+    return os;
+  }
+
+  // Read until double newline or eof
+  friend std::istream &operator>>(std::istream &is, Grid<char> &g) {
+    g.v.emplace_back();
+    while (true) {
+      if (is.peek() == '\n' && g.v.empty())
+        break;
+      if (is.peek() == '\n' && g.v.back().empty() || is.eof()) {
+        g.v.pop_back();
+        break;
+      }
+
+      if (is.peek() == '\n') {
+        is.ignore();
+        g.v.emplace_back();
+      } else {
+        g.v.back().emplace_back();
+        is >> g.v.back().back();
+      }
+    }
+    return is;
+  }
 };
-
-Grid<char> gridify(std::istream &input);
-
-std::ostream &operator<<(std::ostream &os, const Grid<char> g);
