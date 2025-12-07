d89eb8c73d8775330f8197bdc249ef2ae33bbbab
Liam van der Vyver <91105007+liamvdvyver@users.noreply.github.com>
2025-12-08 09:44:50 +1100

    refactor: clean up day 1

--- solutions-25/day_01/day_01.cpp
+++ solutions-25/day_01/day_01.cpp
@@ -1,30 +1,36 @@
-#include <string>
+#include <chrono>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
+  auto start_time = chrono::steady_clock::now();
 
-    int ret_01 = 0;
-    int ret_02 = 0;
+  uint64_t ret_01 = 0;
+  uint64_t ret_02 = 0;
 
-    int mod = 100;
-    int cur = 50;
+  constexpr int64_t mod = 100;
+  int64_t cur = 50;
 
-    for (std::string ln; cin >> ln; ) {
+  for (std::string ln; getline(cin, ln);) {
 
-        int sign = ln[0] == 'R' ? +1 : -1;
-        int n = stoi(ln.substr(1));
-        int prev = cur;
+    int64_t sign = ln[0] == 'R' ? +1 : -1;
+    int64_t n = stoi(ln.substr(1));
+    int64_t prev = cur;
 
-        cur += (n * sign);
+    cur += (n * sign);
 
-        ret_02 += cur > 0 ? cur / mod : ((prev != 0) + (abs(cur) / mod));
+    ret_02 += cur > 0 ? cur / mod : ((prev != 0) + (abs(cur) / mod));
 
-        cur = ((cur % mod) + mod) % mod;
+    cur = ((cur % mod) + mod) % mod;
 
-        if (!cur) ret_01++;
-    }
+    if (!cur)
+      ret_01++;
+  }
 
-    cout << ret_01 << '\n' << ret_02 << '\n';
+  cout << ret_01 << '\n' << ret_02 << '\n';
+
+  auto end_time = chrono::steady_clock::now();
+  cerr << chrono::duration<double, milli>(end_time - start_time) << '\n';
 }
