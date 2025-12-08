2fbc92c601539faae70ad5e3e1a1dc103de607e0
Liam van der Vyver <91105007+liamvdvyver@users.noreply.github.com>
2025-12-08 11:07:54 +1100

    refactor: clean up day 2

--- solutions-25/day_02/day_02.cpp
+++ solutions-25/day_02/day_02.cpp
@@ -1,44 +1,59 @@
-#include <cmath>
+#include <chrono>
 #include <cstdint>
 #include <iostream>
-#include <string>
 #include <unordered_set>
 
 using namespace std;
 
-uint64_t check_range_p1(string &start, string &end) {
-  uint64_t l = stoull(start);
-  uint64_t h = stoull(end);
+template <typename T> constexpr T dec_len(T x) {
+  T ret = 0;
+  for (; x; ret++, x /= 10) {
+  };
+  return ret;
+}
 
+template <typename T> constexpr T ten_msk(T x) {
+  T ret = 1;
+  for (; x; ret *= 10, x /= 10) {
+  };
+  return ret;
+}
+
+template <typename T> constexpr T half_ten_msk(T x) {
+  T ret = 1;
+  for (; x; ret *= 10, x /= 100) {
+  };
+  return ret;
+}
+
+constexpr uint64_t check_range_p1(uint64_t l, const uint64_t h) {
   uint64_t ret = 0;
 
-  uint64_t cur_half =
-      stoull(start.substr(0, max((size_t)1, start.length() / 2)));
-  uint64_t cur = stoull(to_string(cur_half) + to_string(cur_half));
+  uint64_t cur_half = l / half_ten_msk(l);
+  uint64_t cur_half_ten_msk = ten_msk(cur_half);
+  uint64_t cur = cur_half * cur_half_ten_msk + cur_half;
 
-  while (cur <= h) {
+  while ((cur = cur_half * cur_half_ten_msk + cur_half) <= h) {
     if (cur >= l)
       ret += cur;
     cur_half++;
-    cur = stoull(to_string(cur_half) + to_string(cur_half));
+
+    if (cur_half / cur_half_ten_msk)
+      cur_half_ten_msk = ten_msk(cur_half);
   }
   return ret;
 }
 
-uint64_t check_range_p2(string &start, string &end) {
+constexpr uint64_t check_range_p2(const uint64_t l, const uint64_t h) {
 
   std::unordered_set<uint64_t> seen;
 
-  uint64_t l = stoull(start);
-  uint64_t h = stoull(end);
-
   uint64_t ret = 0;
+  const uint64_t end_len = dec_len(h) / 2;
 
   // Length of numeric subsequence
-  for (uint64_t len = 1; len <= end.length() / 2; len++) {
-
-    uint64_t pow_l = pow(10, len - 1);
-    uint64_t pow_h = 10 * pow_l;
+  for (uint64_t len = 1, pow_l = 1, pow_h = 10; len <= end_len;
+       len++, pow_l *= 10, pow_h *= 10) {
 
     // Each sequence of this length
     for (uint64_t seq = pow_l; seq < pow_h; seq++) {
@@ -58,22 +73,23 @@ uint64_t check_range_p2(string &start, string &end) {
 }
 
 int main(void) {
-  string start;
-  string end;
+  auto start_time = chrono::steady_clock::now();
 
   uint64_t ret_1 = 0;
   uint64_t ret_2 = 0;
 
-  while (getline(cin, start, '-') && getline(cin, end, ',')) {
+  char ignore;
+  uint64_t l, h;
+  while (cin >> l >> ignore >> h) {
 
-    // Remove newline
-    if (end[end.length() - 1] == '\n') {
-      end = end.substr(0, end.length() - 1);
-    }
+    ret_1 += check_range_p1(l, h);
+    ret_2 += check_range_p2(l, h);
 
-    ret_1 += check_range_p1(start, end);
-    ret_2 += check_range_p2(start, end);
+    cin.ignore();
   }
 
   cout << ret_1 << '\n' << ret_2 << '\n';
+
+  auto end_time = chrono::steady_clock::now();
+  cerr << chrono::duration<double, milli>(end_time - start_time) << '\n';
 }
